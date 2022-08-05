aa6503f43465bccad6435c3299a8794d1a147bd8
Karlex1 <104476272+Karlex1@users.noreply.github.com>
2022-08-05 09:51:21 +0530

    api function and result visible

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
-
+import { apiGet } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
-
+  const [results, setResults] = useState(null);
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
+    apiGet(`/search/shows?q=${input}`).then(result => {
+        setResults(result);
+       // console.log(result);
       });
   };
 
@@ -21,7 +20,18 @@ const Home = () => {
       onSearch();
     }
   };
-
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Result</div>
+    }
+    if (results && results.length > 0) {
+      return <div >{results.map((item) =>
+        <div key={item.show.id}>
+          {item.show.name}
+        </div>)}</div>
+    }
+    return null;
+  };
   return (
     <MainPageLayout>
       <input
@@ -34,6 +44,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
