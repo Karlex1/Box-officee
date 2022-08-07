1c886a65c88c1c7c165bae1641044e64837604c3
Karlex1 <104476272+Karlex1@users.noreply.github.com>
2022-08-07 11:37:09 +0530

    useReducer

--- src/pages/show.js
+++ src/pages/show.js
@@ -1,25 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect,useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+const initialstate = {
+    show: null,
+    isLoading: true,
+    error: null
+}
+const reducer = (prevState,action) => {
+    switch (action.type) {
+        case 'FETCH_SUCESS': {
+            return{isLoading:false,show:action.show,error:null}
+        }
+        case 'FETCH_FAILED': {
+            return {...prevState,
+              isLoading:false,error:action.error
+            }
+            }
+        default: return prevState;
+    }
+}
+
 const Show = () => {
-    const [show, setShow] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+
+   const [{isLoading,error,show},dispatch]= useReducer(reducer,initialstate);
+    // const [show, setShow] = useState(null);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [error, setError] = useState(null);
     const { id } = useParams();
     useEffect(
         () => {
             let isMounted = 'true';
-            apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+            apiGet(`/shows/${id}?embed[]=seasons&embed7[]=cast`).then(results => {
                 if (isMounted) {
-                   setShow(results);
-                setIsLoading(false); 
-                }
-                
-            }).catch(
-                err => {
-                    setError(err.message);
-                    setIsLoading(false);
+                    dispatch({type:'FETCH_SUCESS',show:results})
+                //    setShow(results);
+                // setIsLoading(false); 
+                }}).catch(
+                    err => {
+                        dispatch({ type: 'FETCH_FAILED', show: err.message })
+                    // setError(err.message);
+                    // setIsLoading(false);
                 })
             return () => { isMounted = false; }
         }, [id])
