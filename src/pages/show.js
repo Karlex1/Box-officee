21ccca17e024340dda3cfada7a2bf5ddc13834c1
Karlex1 <104476272+Karlex1@users.noreply.github.com>
2022-08-07 09:14:14 +0530

    some more changes

--- src/pages/show.js
+++ src/pages/show.js
@@ -1,18 +1,41 @@
-import React, { useEffect ,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 const Show = () => {
     const [show, setShow] = useState(null);
-const {id} =useParams()
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const { id } = useParams();
     useEffect(
         () => {
-            apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => { setShow(results); })
+            let isMounted = 'true';
+            apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+                if (isMounted) {
+                   setShow(results);
+                setIsLoading(false); 
+                }
+                
+            }).catch(
+                err => {
+                    setError(err.message);
+                    setIsLoading(false);
+                })
+            return () => { isMounted = false; }
         }, [id])
- console.log('show',show);
-  return (
-    <div>show</div>
-  )
+    console.log('show', show);
+    if (isLoading) {
+       return <div>Loading...</div>
+    }
+    if (error) {
+        return <div>Error occured: { error}</div>
+    }
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+    return (
+        <div>show</div>
+    )
 }
 
 export default Show
