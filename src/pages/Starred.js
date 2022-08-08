55014f95b8729d953962c03935f3da00922d8dbf
Karlex1 <104476272+Karlex1@users.noreply.github.com>
2022-08-09 02:04:38 +0530

    starred page completed

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import MainPageLayout from '../components/MainPageLayout';
-
+import { apiGet } from '../misc/config';
+import { useShows } from "../misc/customHooks";
+import ShowGrid from '../shows/ShowsGrid';
 const Starred = () => {
-  return <MainPageLayout>this is starred</MainPageLayout>;
+  const [starred] = useShows();
+
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+      // the reason of using Promise.all is that we r using a API which doesn't give us a endpoint where  we can send request to fetch multiple data (in one request).
+      Promise.all(promises).then(apiData => apiData.map(show => ({ show }))).then(results => {
+        setShows(results);
+        setIsLoading(false);
+      }).catch(err => {
+        setIsLoading(false);
+        setError(err.message)
+      });
+    } else { setIsLoading(false); }
+   }, [starred]);
+  return <MainPageLayout>
+    {isLoading && <div>Shows are still loading</div>}
+    {error && <div>Error occured: {error}</div>}
+    {!isLoading && !shows && <div>No shows were added</div>}
+    {!isLoading && !error && shows && <ShowGrid data={shows} />}
+  </MainPageLayout>;
 };
 
 export default Starred;
