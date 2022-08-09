a9ad795e744082f587d41aac28512c0c7daf564e
Karlex1 <104476272+Karlex1@users.noreply.github.com>
2022-08-09 17:07:45 +0530

    Updates

---
+++ precache-manifest.dfb122bf4423333164f85354364f27f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87672aa991131cac4363eda0fd4c7ead",
+    "url": "/Box-officee/index.html"
+  },
+  {
+    "revision": "135ea3259b6ac855e0d4",
+    "url": "/Box-officee/static/css/main.eb0d8233.chunk.css"
+  },
+  {
+    "revision": "d98569a307c5e2f7200d",
+    "url": "/Box-officee/static/js/2.0ea70939.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-officee/static/js/2.0ea70939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135ea3259b6ac855e0d4",
+    "url": "/Box-officee/static/js/main.3eac6c20.chunk.js"
+  },
+  {
+    "revision": "1ffecb4018aed5bdc551",
+    "url": "/Box-officee/static/js/runtime-main.7248509b.js"
+  }
+]);
