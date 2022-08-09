56ff669071c564b9fb13aae127794574fe666d7b
Karlex1 <104476272+Karlex1@users.noreply.github.com>
2022-08-09 16:42:28 +0530

    fadein animation

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import FadeIn from "react-fade-in";
 
-export const Flexgrid = styled.div`
+export const Flexgrid = styled(FadeIn)`
 display:flex;
 justify-content:center;
 flex-wrap:wrap;
